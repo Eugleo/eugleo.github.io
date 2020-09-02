d77828305f637e9e174148fd4db6fd7bf337dbdd
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-09-02 15:47:50 +0200

    Add redirects

--- next.config.js
+++ next.config.js
@@ -10,4 +10,20 @@ const mdx = require('@next/mdx')({
 
 const nextConfig = { pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'] };
 
-module.exports = withPlugins([mdx], nextConfig);
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/prog1',
+        destination: '/teaching/2020-2021/prog1',
+        permanent: false,
+      },
+      {
+        source: '/prog2',
+        destination: '/teaching/2020-2021/prog2',
+        permanent: false,
+      },
+    ];
+  },
+  plugins: withPlugins([mdx], nextConfig),
+};
