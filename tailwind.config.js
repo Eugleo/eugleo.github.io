308385a4dd4b6148fd33104843cb1382d3258345
Evžen Wybitul <wybitul.evzen@gmail.com>
2020-09-04 21:38:11 +0200

    Fix bug with tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   purge: [
     './src/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
-    './posts/**/*.{js,ts,jsx,tsx}',
+    './posts/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
